488426ed0707db5dfdd3ea11cfeecc29fed4ea26
Natalie Montoya <nataliesbarron@gmail.com>
2017-12-06 13:50:36 -0700

    Write test for player telport

--- test/Player-test.js
+++ test/Player-test.js
@@ -152,13 +152,25 @@ describe('Player Collisions', function() {
 
 describe('Player mechanics', function() {
   
-  it('should teleport across the screen', function() {
+  it('should teleport across the screen left to right', function() {
     let p = new Player(1, 500, 0, 0, 32, 32);
     p.teleport(800);
     assert.equal(p.x, 1); //before teleport
 
     p.x = -1;
+    p.teleport(800);
     assert.equal(p.x, 772); //after teleport
-
   })
+
+  it('should teleport across the screen right to left', function() {
+    let p = new Player(772, 500, 0, 0, 32, 32);
+    p.teleport(800);
+    assert.equal(p.x, 772); //before teleport
+
+    p.x = 773 + p.width;
+    p.teleport(800);
+    assert.equal(p.x, 0); //after teleport
+  })
+
+  
 })
